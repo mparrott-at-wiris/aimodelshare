00c0132ef63a68e32415fada752cb185a463da77
Michael Parrott <150669552+mparrott-at-wiris@users.noreply.github.com>
2025-11-23 20:49:10 +0100

    Update moral_compass_challenge.py

--- aimodelshare/moral_compass/apps/moral_compass_challenge.py
+++ aimodelshare/moral_compass/apps/moral_compass_challenge.py
@@ -479,3 +479,4 @@ def launch_moral_compass_challenge_app(height: int = 1000, share: bool = False, 
     import os
     port = int(os.environ.get("PORT", 8080))
     demo.launch(share=share, inline=True, debug=debug, height=height, server_port=port)
+
