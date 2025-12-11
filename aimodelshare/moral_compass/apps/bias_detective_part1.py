e844642184c80e1a6d737e49abb87e43374c7669
Michael Parrott <150669552+mparrott-at-wiris@users.noreply.github.com>
2025-12-11 11:05:57 +0100

    feat: Rename function to create_bias_detective_part1_app

--- aimodelshare/moral_compass/apps/bias_detective_part1.py
+++ aimodelshare/moral_compass/apps/bias_detective_part1.py
@@ -1785,7 +1785,7 @@ def simulate_moral_compass_score(acc, progress_pct):
 
 
 # --- 13. APP FACTORY (APP 1) ---
-def create_bias_detective_app(theme_primary_hue: str = "indigo"):
+def create_bias_detective_part1_app(theme_primary_hue: str = "indigo"):
     with gr.Blocks(theme=gr.themes.Soft(primary_hue=theme_primary_hue), css=css) as demo:
         # States
         username_state = gr.State(value=None)
